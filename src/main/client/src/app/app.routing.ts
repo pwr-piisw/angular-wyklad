892ba77c1ce68aa8d98e31d9d1b42a1bcbe3df36
Maciej Malecki <maciej.malecki@pwr.wroc.pl>
2017-11-22 11:57:33 +0100

    Step 3 - Navigation and routing extended with new component

--- src/main/client/src/app/app.routing.ts
+++ src/main/client/src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {BookOverviewComponent} from './book-mgmt/book-overview/book-overview.component';
 import {BookDetailsComponent} from './book-mgmt/book-details/book-details.component';
+import {UserListComponent} from './user-mgmt/user-list/user-list.component';
 
 export const APP_ROUTES: Routes = [
   {
@@ -17,6 +18,10 @@ export const APP_ROUTES: Routes = [
       {
         path: 'book/:bookId',
         component: BookDetailsComponent
+      },
+      {
+        path: 'users',
+        component: UserListComponent
       }
     ]
   },
