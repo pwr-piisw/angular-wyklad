3c02625a124245e50138b109aa15d9a0e471cd40
Maciej Malecki <maciej.malecki@pwr.wroc.pl>
2017-11-22 12:37:10 +0100

    Step 4 - Simple data binding and *ngFor directive example.

--- src/main/client/src/app/user-mgmt/user-list/user-list.component.ts
+++ src/main/client/src/app/user-mgmt/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user.dto';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
+  users: User[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.users = [
+      { id: 1, name: 'John Doe', email: 'john.doe@email.com'},
+      { id: 2, name: 'Max Rockatansky', email: 'mad.max@email.com'},
+      { id: 3, name: 'Chuck Peddle', email: 'chuck@mos.com'}
+    ];
   }
 
 }
